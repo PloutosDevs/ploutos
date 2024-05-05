2fec375e35160dcf788dbec4f3ec1bcf5033348a
Герман <g.d-9898@mail.ru>
2024-05-06 00:29:25 +0300

    Changed eval logic for new features Changed defining pump, now pump is yield during period rather than after period Changed config

--- source/data/process/compose_features.py
+++ source/data/process/compose_features.py
@@ -47,14 +47,20 @@ def create_lag_features(data, column_names, lag=1):
 
 def add_features(data: pd.DataFrame, exp_config) -> pd.DataFrame:
 
-    feature_cols = sum(list(map(lambda x: x[0], exp_config['features'].values())), []) 
-    data_set = pd.DataFrame()
-    symbols = data["Symbol"].unique()
+    symbol_features_cols = sum(
+        list(map(lambda x: x[0], exp_config['features'].get("symbol_features", {}).values())), []
+    )
+    dates_features_cols = sum(
+        list(map(lambda x: x[0], exp_config['features'].get("dates_features", {}).values())), []
+    )
+    feature_cols = symbol_features_cols + dates_features_cols
 
-    for symbol in tqdm(symbols):
+    upd_data = []
+
+    for symbol in tqdm(data["Symbol"].unique()):
         
         # Get sample of prices by ticker
-        sample = data[data["Symbol"] == symbol]
+        sample = data[data["Symbol"] == symbol].copy()
         
         if sample.shape[0] < 10:
             continue
@@ -66,15 +72,46 @@ def add_features(data: pd.DataFrame, exp_config) -> pd.DataFrame:
 
         # Calculate trailing cumulative yield over specified rolling window
         # Calculate yield at 1 day before cumulative yield. In other words yield_before_pump is not included in calculation of cum_prod, yield_before_pump == before pump date
-        sample.loc[:, "cum_prod"] = sample["Close"].pct_change().add(1).rolling(window=exp_config['strategy_params']["validation_window"]).apply(np.prod).subtract(1).multiply(100).fillna(0).values
-        sample.loc[:, "yield_before_pump"] = sample["Close"].pct_change().multiply(100).shift(exp_config['strategy_params']["validation_window"])
+        sample.loc[:, "cum_prod"] = (
+            sample["Close"].pct_change()
+                           .add(1)
+                           .rolling(window=exp_config['strategy_params']["validation_window"])
+                           .apply(lambda x: x.cumprod().max())  # np.prod
+                           .subtract(1)
+                           .multiply(100)
+                           .fillna(0)
+                           .values
+        )
+        sample.loc[:, "yield_before_pump"] = (
+            sample["Close"].pct_change().multiply(100).shift(exp_config['strategy_params']["validation_window"])
+        )
 
-        # Calculate features. Feature functions update original DataFrame
-        for feature, values in exp_config['features'].items():
-            eval(feature)(sample, **values[1])
-        
+        # Calculate features relates to symbol
+        for feature, values in exp_config['features'].get("symbol_features", {}).items():
+            sample = eval(feature)(sample, **values[1])
+
+        upd_data.append(sample.copy())
+
+    upd_data = pd.concat(upd_data)
+
+    # Calculate features relates to dates
+    for feature, values in exp_config['features'].get("dates_features", {}).items():
+        upd_data = eval(feature)(upd_data, **values[1])
+
+    data_set = []
+
+    # Calculate lags
+    for symbol in tqdm(upd_data["Symbol"].unique()):
+
+        sample = upd_data[upd_data["Symbol"] == symbol]
+
         feature_df = sample.bfill()
-        feature_df = create_lag_features(feature_df, feature_cols, lag=exp_config['strategy_params']['last_features_window'])
-        data_set = pd.concat([data_set, feature_df])
-        
+        feature_df = create_lag_features(
+            feature_df, feature_cols, lag=exp_config['strategy_params']['last_features_window']
+        )
+
+        data_set.append(feature_df.copy())
+
+    data_set = pd.concat(data_set)
+
     return data_set
--- source/model/eval.py
+++ source/model/eval.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 
 def eval_model(valuation_date: pd.Timestamp = pd.Timestamp.today(tz=config.DEFAULT_TZ).normalize(),
-               candles_period: int = 60, best_symbols_offset: int = 50, yield_before_pump: float = 0.03):
+               candles_period: int = 60, best_symbols_offset: int = 50):
 
     # Get data
     print('Start to get candels')
@@ -20,41 +20,65 @@ def eval_model(valuation_date: pd.Timestamp = pd.Timestamp.today(tz=config.DEFAU
 
     eval_candles_df = compose_binance_candles_df(BINANCE_SYMBOLS, start_time=candles_start_date,
                                                  end_time=candles_end_date)
-    
+
     EXPERIMENT_CONFIG = {
         'features': {
-            "calculate_supertrend": [["SuperTrend"], {"vol_func": "atr", "period": 20, "multiplier": 2.5}],
-            "calculate_macd": [
-                ["MACD_Signal_Line", "MACD", "MACD_Bar_Charts"],
-                {"short_period": 12, "long_period": 26, "smoothing_period": 9}
-            ],
-            "calculate_rsi": [["RSI"], {"period": 20, "ema": True}],
-            "calculate_obv_to_volume_ratio": [["OBV_Volume_Ratio"], {}],
-            "distance_between_bb_bands": [
-                ["Upper_distance", "Lower_distance"],
-                {"period": 20, "multiplier": 2.5, "ema": 2.5, "normalize": True}
-            ],
-            "calculate_cmf": [["CMF"], {"period": 20}],
-            "calculate_price_rate_of_change": [["Price_ROC"], {}],
-            "calculate_volume_rate_of_change": [["Volume_ROC"], {}],
-            "calculate_volume_ratio": [["Volume_Ratio"], {"period": 20, "ema": True}],
-            "calculate_stoch_rsi": [
-                ["Stoch_RSI_K", "Stoch_RSI_D"],
-                {"rsi_period": 20, "k_period": 20, "smooth_k": 5, "smooth_k": 5, "ema": True}
-            ],
-            "calculate_trailing_linear_reg_params": [["Reg_Coef", "RMSE"], {"period": 25, "col_name": "cum_prod"}]
+            "symbol_features": {
+                "calculate_supertrend": [["SuperTrend"], {"vol_func": "atr", "period": 20, "multiplier": 2.5}],
+                "calculate_macd": [
+                    ["MACD_Signal_Line", "MACD", "MACD_Bar_Charts"],
+                    {"short_period": 12, "long_period": 26, "smoothing_period": 9}
+                ],
+                "calculate_rsi": [["RSI"], {"period": 20, "ema": True}],
+                "calculate_obv_to_volume_ratio": [["OBV_Volume_Ratio"], {}],
+                "distance_between_bb_bands": [
+                    ["Upper_distance", "Lower_distance"],
+                    {"period": 20, "multiplier": 2.5, "ema": 2.5, "normalize": True}
+                ],
+                "calculate_cmf": [["CMF"], {"period": 20}],
+                "calculate_price_rate_of_change": [["Price_ROC"], {}],
+                "calculate_volume_rate_of_change": [["Volume_ROC"], {}],
+                "calculate_volume_ratio": [["Volume_Ratio"], {"period": 20, "ema": True}],
+                "calculate_stoch_rsi": [
+                    ["Stoch_RSI_K", "Stoch_RSI_D"],
+                    {"rsi_period": 20, "k_period": 20, "smooth_k": 5, "smooth_k": 5, "ema": True}
+                ],
+                "calculate_trailing_linear_reg_params": [["Reg_Coef", "RMSE"], {"period": 25, "col_name": "cum_prod"}],
+                "calculate_fibonacci_levels": [
+                    ["fibo_23.6", "fibo_38.2", "fibo_50", "fibo_61.8", "fibo_100"],
+                    {"period": 21, "type_deal": "long"}
+                ]
+            },
+            "dates_features": {
+                "calculate_fear_and_greed_index": [["fear_and_greed"], {}],
+                "calculate_dominance": [["btc_dominance", "eth_dominance"], {}],
+                "calculate_btc_features": [
+                    [
+                        'btc_SuperTrend', 'btc_MACD_Signal_Line', 'btc_MACD', 'btc_MACD_Bar_Charts', 'btc_RSI',
+                        'btc_OBV_Volume_Ratio', 'btc_Upper_distance', 'btc_Lower_distance', 'btc_CMF', 'btc_Price_ROC',
+                        'btc_Volume_ROC', 'btc_Volume_Ratio', 'btc_Stoch_RSI_K', 'btc_Stoch_RSI_D', 'btc_Reg_Coef',
+                        'btc_RMSE'
+                    ],
+                    {}
+                ]
+            }
         },
         'strategy_params': {
             "last_features_window": 7,
-            "candles_between_pump": 30,
+            "candles_between_pump": 25,
             "validation_window": 20,  # candles
             "min_yield": -20,  # %
-            "max_yield": 20,
-            "first_yield": 3
+            "max_yield": 20,  # %
+            "first_yield": 3  # %
+        },
+        'data_processing': {
+            "sample_multiplier": 2.5
         }
     }
 
-    eval_candles_df = utils.filter_symbols_by_last_yield(eval_candles_df, yield_offset=yield_before_pump)
+    eval_candles_df = utils.filter_symbols_by_last_yield(
+        eval_candles_df, yield_offset=EXPERIMENT_CONFIG["strategy_params"]["first_yield"] / 100
+    )
 
     if eval_candles_df.empty:
         return b"", []
